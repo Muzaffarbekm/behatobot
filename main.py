13263ac2bd200ada1a53cc059d508d87bfe2c4fb
Muzaffarbekm <71602207+Muzaffarbekm@users.noreply.github.com>
2021-08-20 00:26:34 +0500

    Added spelling checker and modified wordchecker.py
    Spelling checker can show similar results and show whether it is right or wrong . If the given word is right then just returns ✅

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from contextvars import Token
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types.base import T
 from wordChecker import checker
 from local_settings import token
 
@@ -18,9 +17,23 @@ dp = Dispatcher(bot)
 async def welcome(message: types.Message):
     await message.reply("Behato botiga xush kelibsiz!")
     
+    
 @dp.message_handler(commands='help')
 async def help(message: types.Message):
     await message.reply("Botni ishlatish uchun so'z yuboring")
+    
+    
+@dp.message_handler()
+async def checkSpelling(message: types.Message):
+    word = message.text
+    result = checker(word)
+    if result['available']:
+        response = f"✅ {word.capitalize()}"
+    else:
+        response = f"❌{word.capitalize()}\n"
+        for text in result['matches']:
+            response += f"✅ {text.capitalize()}\n"
+    await message.answer(response)       
     
     
 if __name__ == '__main__':
--- wordChecker.py
+++ wordChecker.py
@@ -2,23 +2,27 @@ from uzwords import words
 from difflib import get_close_matches
 
 
-def checker(word, words = words):
-    word = word.lower
-    match = set(get_close_matches(word, words))
-    available = False
+def checker(word,words=words):
+    word = word.lower()
+    matches = set(get_close_matches(word, words))
+    available = False # There are no such words
     
-    if word in match:
-        available = True # mavjud
+    if word in matches:
+        available = True # There are such words
         matches = word
     elif 'ҳ' in word:
         word = word.replace('ҳ', 'х')
-        match.update(get_close_matches(word, words))
+        matches.update(get_close_matches(word, words))
     elif 'х' in word:
         word = word.replace('х', 'ҳ')
-        match.update(get_close_matches(word, words))
+        matches.update(get_close_matches(word, words))
     
-    return { "available": available ,"match": match }
+    return { "available": available ,"matches": matches }
 
 if __name__ == '__main__':
-    print("hello world")
-    print(checker("тариҳ"))
+    print(checkWord("ҳато"))
+    print(checkWord("тариҳ"))
+    print(checkWord("хато"))
+    print(checkWord("олма"))
+    print(checkWord("ҳат"))
+    print(checkWord("ҳайт"))
