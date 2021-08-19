23878c7d4ea638b37e15c0c8a878f43279a1def9
Muzaffarbekm <71602207+Muzaffarbekm@users.noreply.github.com>
2021-08-20 00:04:59 +0500

    Revert "Revert "wordchecker.py modified""
    This reverts commit 0b2212f08d7d93dcc064a613dda4d154b8c37555.

--- wordChecker.py
+++ wordChecker.py
@@ -8,8 +8,17 @@ def checker(word, words = words):
     available = False
     
     if word in match:
-        available = True
-        match = word
+        available = True # mavjud
+        matches = word
+    elif 'ҳ' in word:
+        word = word.replace('ҳ', 'х')
+        match.update(get_close_matches(word, words))
+    elif 'х' in word:
+        word = word.replace('х', 'ҳ')
+        match.update(get_close_matches(word, words))
     
     return { "available": available ,"match": match }
 
+if __name__ == '__main__':
+    print("hello world")
+    print(checker("тариҳ"))
