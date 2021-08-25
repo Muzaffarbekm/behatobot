0519196e919d266423e5ccb0a4d5010855b03bf1
Muzaffarbekm <71602207+Muzaffarbekm@users.noreply.github.com>
2021-08-25 23:37:14 +0500

    texthandler added
    text handler can automatically answer to the questions or any messages

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -15,13 +15,14 @@ logging.basicConfig(level=logging.INFO)
 
 # bot = Bot(token=BOT_TOKEN)
 # dp = Dispatcher(bot)
-@dp.message_handler(CommandStart(deep_link='https://t.me/helloworld0071'))
+
+@dp.message_handler(CommandStart(deep_link='hello'))
 async def welcome(message: types.Message):
     info = message.get_args()
     text = f"Assalomu alaykum {message.from_user.full_name}!\n Siz {info} orqali qoshildizngiz!"   
     await message.reply(text)
     
-    
+   
 @dp.message_handler(commands='start')
 async def welcome(message: types.Message):
     info = message.get_args()
