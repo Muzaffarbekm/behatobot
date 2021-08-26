2fce79365702d97e425468dfb9559330481440d9
Muzaffarbekm <71602207+Muzaffarbekm@users.noreply.github.com>
2021-08-27 00:24:01 +0500

    Updated otherhandlers.py
    Added fns to handle forwarded msgs, if user sending his contact and /private msging with bot

--- handlers/users/otherhandlers.py
+++ handlers/users/otherhandlers.py
@@ -1,8 +1,20 @@
 from aiogram import types
 from loader import dp
-
+from aiogram.dispatcher import filters
 
 @dp.message_handler(is_reply=True,commands="user_id")
 async def identify_id(message: types.Message):
     ids = message.from_user.id
     await message.reply(ids)
+    
+@dp.message_handler(content_types="contact",is_sender_contact=True)
+async def contat_send(message: types.Message):
+    await message.reply("Kontaktingiz qabul qilindi!")
+    
+@dp.message_handler(is_forwarded=True)
+async def contat_send(message: types.Message):
+    await message.reply("Forward qilish mumkin emas!")   
+    
+@dp.message_handler(filters.ChatTypeFilter(types.ChatType.PRIVATE), commands="private") # if user writes /private go ahead
+async def contat_send(message: types.Message):
+    await message.reply("Siz shaxsiy habarlarga o'tdingiz!") 
