7901d69474484182d0b2cbcd00e9d3aa25957fc1
Muzaffarbekm <71602207+Muzaffarbekm@users.noreply.github.com>
2021-08-26 23:49:15 +0500

    added state to the search command
    added state to the search command to prevent from doing other random things . And ask user to search smth

--- handlers/users/regexhandler.py
+++ handlers/users/regexhandler.py
@@ -1,11 +1,12 @@
 from aiogram import types
-from aiogram.dispatcher.filters import Regexp
+from aiogram.dispatcher.filters import Regexp,RegexpCommandsFilter
 from loader import dp
 
 
 EMAIL_REGEX = r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
 PHONE_REGEX = r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$'
-START_WITH_EMAIL = r'email:' + EMAIL_REGEX
+IP = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}'
+START_WITH_EMAIL = r"email:" + EMAIL_REGEX
 
 @dp.message_handler(Regexp(EMAIL_REGEX))
 async def email(message: types.Message):
@@ -16,4 +17,9 @@ async def email(message: types.Message):
 async def email(message: types.Message):
     name = message.from_user.full_name
     await message.reply(f"{name} telefon raqamingiz qabul qilindi, tez orada operator aloqaga chiqadi!")
+    
+@dp.message_handler(regexp_commands=[IP])
+async def start_with_email(message: types.Message):
+    name = message.from_user.full_name
+    await message.reply(f"emailingiz qabul bo'ldi, tez orada javob yozamiz!")
     
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,17 +1,24 @@
 import logging
 
 from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.types.callback_query import CallbackQuery
 from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 from contextvars import Token
 from aiogram import Bot, Dispatcher, executor, types
 from wordChecker import checker
 from data.config import ADMINS,BOT_TOKEN 
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 BOT_TOKEN = BOT_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
+
+class Form(StatesGroup):
+    search = State()
+    
 
 # bot = Bot(token=BOT_TOKEN)
 # dp = Dispatcher(bot)
@@ -26,7 +33,7 @@ async def welcome(message: types.Message):
 @dp.message_handler(commands='start')
 async def welcome(message: types.Message):
     info = message.get_args()
-    text = f"Assalomu alaykum {message.from_user.full_name}!\nBehato botiga xush kelibsiz"   
+    text = f"Assalomu alaykum {message.from_user.full_name}!\nBehato botiga xush kelibsiz"
     await message.reply(text)
     
 @dp.message_handler(content_types=types.ContentType.PHOTO)
@@ -35,8 +42,10 @@ async def photo_handler(message: types.Message):
     text = f"Xurmatli {message.from_user.full_name}!\n Buni nimaligiga aniqlik kiritsak?"   
     await message.reply(text)    
     
-@dp.message_handler()
-async def checkSpelling(message: types.Message):
+@dp.message_handler(commands='search')
+async def checkSpelling(message: types.Message,state=FSMContext):
+    await Form.search.set()
+
     word = message.text
     result = checker(word)
     if result['available']:
@@ -46,6 +55,11 @@ async def checkSpelling(message: types.Message):
         for text in result['matches']:
             response += f"✅ {text.capitalize()}\n"
     await message.answer(response)       
+ 
+@dp.message_handler(state=Form.search)
+async def checkSpelling(message: types.Message,state=FSMContext): 
+    await message.reply("Qidirgan so'zingiz bo'yicha javoblar yuqorida berildi")
+    await state.finish()
     
     
 if __name__ == '__main__':
